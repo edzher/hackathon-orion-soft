47bb0f6d441abcc57277fa174876816b658a09c6
Nikolayev Semyon <ivanhoekn@gmail.com>
2025-06-07 22:34:11 +0300

    pre test backend

---
+++ backend/src/main/java/ru/fox/orion/service/VacancyService.java
@@ -0,0 +1,27 @@
+package ru.fox.orion.service;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import ru.fox.orion.model.dto.RequestFilter;
+import ru.fox.orion.model.entity.VacancyEntity;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
+
+public interface VacancyService {
+
+    List<String> distinctCities();
+
+    List<String> distinctCompanies();
+
+    Page<VacancyEntity> dashboardData(RequestFilter request, Pageable pageable);
+    
+    Map<String, Object> getSalaryStatistics(String city);
+    
+    List<Map<String, Object>> getJobTrends(LocalDate startDate);
+    
+    List<Map<String, Object>> getTopCompanies();
+    
+    List<Map<String, Object>> getSkillsDemand();
+}
